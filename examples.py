bd100e0ff54fc3ec7bef647df4cdf78da58a8cbe
Lithak <Lithan71@gmail.com>
2020-09-25 11:27:54 +0200

    Else If Statements

---
+++ examples.py
@@ -0,0 +1,13 @@
+mark = int(input("Please enter your mark:"))
+
+if mark>=80 and mark<=100:
+    print("Grade A")
+elif mark>=70 and mark<=79:
+    print("Grade B")
+elif mark>=50 and mark<=69:
+    print("Grade C")
+elif mark>=40 and mark<=49:
+    print("Grade D")
+else:
+    print("Gade E")
+
